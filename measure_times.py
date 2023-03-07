773bcfbd47234d5f669e2bd98e70e73b79012624
Justin San Juan <justin.sanjuan@uwaterloo.ca>
2023-03-07 09:47:45 -0500

    chore: update gpt2 data

--- measure_times.py
+++ measure_times.py
@@ -16,8 +16,8 @@ model_names = [
     # 'codebert_base', # 498965601
     # 'albert-xxlarge-v2', # 890450058
     # 'DialoGPT-large', # 3134799287
-    'bart-large', # 1625830197
-    'gpt2-xl', # 6282033981
+    # 'bart-large', # 1625830197
+    # 'gpt2-xl', # 6282033981
     't5-3B', # 11408097021
     # 't5-small',
     ]
